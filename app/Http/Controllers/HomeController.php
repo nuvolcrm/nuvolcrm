befc06b58c24222f16c96ab060bbfb70f8aa6d05
PC-VICTOR\vmarc <vmarch99@gmail.com>
2021-03-10 10:38:03 +0100

    Modificaciones en el controlador

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,52 @@ class HomeController extends Controller
     {
         return view('home');
     }
+    public function clientes()
+    {
+        return view('admin.clientes');
+    }
+    public function presupuestos()
+    {
+        return view('admin.presupuestos');
+    }
+    public function ventas()
+    {
+        return view('admin.ventas');
+    }
+    public function pedidos()
+    {
+        return view('admin.extra.pedidos');
+    }
+    public function enlaces()
+    {
+        return view('admin.extra.enlaces');
+    }
+    public function frecuentes()
+    {
+        return view('admin.extra.frecuentes');
+    }
+    public function eliminador()
+    {
+        return view('admin.extra.eliminador');
+    }
+    public function operadores()
+    {
+        return view('admin.gestion.operadores');
+    }
+    public function servicios()
+    {
+        return view('admin.gestion.servicios');
+    }
+    public function tarifas()
+    {
+        return view('admin.gestion.tarifas');
+    }
+    public function colaboradores()
+    {
+        return view('admin.gestion.colaboradores');
+    }
+    public function usuarios_roles()
+    {
+        return view('admin.gestion.usuarios_roles');
+    }
 }
