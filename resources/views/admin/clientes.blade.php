9c0897a1fe7a39ffdd0c77a07cab80f9376dcb25
PC-VICTOR\vmarc <vmarch99@gmail.com>
2021-03-10 15:47:13 +0100

    clientes

--- resources/views/admin/clientes.blade.php
+++ resources/views/admin/clientes.blade.php
@@ -1,15 +1,14 @@
 @extends('adminlte::page')
 
-@section('title', 'Clientes')
+@section('title', 'Presupuestos')
 
 @section('content_header')
-<div class="col-sm-4">
     <h1>Clientes</h1>
-    
-    <a href="clientes_nuevo.php" class="btn btn-success">Nuevo Cliente</a>
-</div>
 @stop
 
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
