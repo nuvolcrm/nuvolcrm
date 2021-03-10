dd390010ada522cc670f13d98db30c6646706e46
PC-VICTOR\vmarc <vmarch99@gmail.com>
2021-03-10 13:40:05 +0100

    nombre

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Más</b>Móvil',
+    'logo' => '<b>Nuvol</b>CRM',
     'logo_img' => 'vendor/adminlte/dist/img/masmovil.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
