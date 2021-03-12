1aa7ce35a6f79fbe652fa79e2bf2106c172ed038
PC-VICTOR\vmarc <vmarch99@gmail.com>
2021-03-12 23:04:37 +0100

    no message

--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ClientesController extends Controller
@@ -17,5 +18,38 @@ class ClientesController extends Controller
         $clientes = DB::table('cliente')->get();
 
         return view('resources.views.admin.clientes', ['clientes' => $clientes]);
+    }
+    public function store(Request $request){
+
+        $clientes = array("dni"=>$request->input("dni"),
+                          "nombre"=>$request->input("nombre"),
+                          "apellido1"=>$request->input("apellido1"),
+                          "apellido2"=>$request->input("apellido2"),
+                          "alias"=>$request->input("alias"),
+                          "fecha_nacimiento"=>$request->input("f_nacimiento"),
+                          "direccion"=>$request->input("direccion"),
+                          "poblacion"=>$request->input("poblacion"),
+                          "telefono"=>$request->input("telefono"),
+                          "idioma"=>$request->radio("idioma"),
+                          "email"=>$request->input("email"),
+                          "segmento"=>$request->radio("segmento"));
+
+        $cliente = new Cliente();
+        $clietne->dni = $clientes["dni"];
+        $clietne->nombre = $clientes["nombre"];
+        $clietne->apellido1 = $clientes["apellido1"];
+        $clietne->apellido2 = $clientes["apellido2"];
+        $clietne->alias = $clientes["alias"];
+        $clietne->fehca_nacimiento = $clientes["f_nacimiento"];
+        $clietne->direccion = $clientes["direccion"];
+        $clietne->poblacion = $clientes["poblacion"];
+        $clietne->telefono = $clientes["telefono"];
+        $clietne->idioma = $clientes["idioma"];
+        $clietne->email = $clientes["email"];
+        $clietne->segmento = $clientes["segmento"];
+
+        $cliente->save();
+
+        return redirect("/clientes")-with("ok-crear","");
     }
 }
---
+++ resources/views/admin/crear_clientes.blade.php
@@ -0,0 +1,121 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Pedidos')
+
+@section('content_header')
+    <h1>Crear Cliente</h1>
+@stop
+
+@section('content')
+    <form action="../app/Http/Controllers/ClientesController.php" method="POST">
+        @csrf
+        <div class="input-group">
+            <div>
+                <label class="control-label"> DNI</label>
+                <input  type="text" name="dni" placeholder="DNI" class="form-control mb-2" required>
+            </div>
+            <div>
+                <label class="control-label"> Nombre</label>
+                <input style="margin-left: 5%" type="text" name="nombre" placeholder="Nombre" class="form-control mb-2" required>
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div>
+                <label class="control-label"> Primer Apellido</label>
+                <input type="text" name="apellido1" placeholder="Primer Apellido" class="form-control mb-2" required>
+            </div>
+            <div>
+                <label class="control-label"> Segundo Apellido</label>    
+                <input style="margin-left: 5%"  type="text" name="apellido2" placeholder="Segundo Apellido" class="form-control mb-2" required>
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div>
+                <label class="control-label"> Alias</label>
+                <input type="text" name="alias" placeholder="Alias" class="form-control mb-2">
+            </div>
+            <div>
+                <label class="control-label"> Fecha de Nacimiento</label>
+                <input style="margin-left: 5%" type="date" name="f_nacimiento" class="form-control mb-2" required>
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div>
+                <label class="control-label"> Telefono</label>
+                <input type="tel" name="telefono" required placeholder="Telefono de Contacto" maxlength="9" pattern="[6-7-9]{1}[0-9]{8}" class="form-control mb-2">
+            </div>
+            <div>
+                <label class="control-label"> Email</label>
+                <input style="margin-left: 5%" type="email" name="email" required placeholder="Introduce el Email" class="form-control mb-2">
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div>
+                <label class="control-label"> Dirección</label>
+                <input size="50%" type="text" name="direccion" placeholder="Dirección" class="form-control mb-2" required> 
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div>
+                <label class="control-label"> Población</label>
+                <input  type="text" name="poblacion" placeholder="Poblacion" class="form-control mb-2" required>
+            </div>
+            <div>
+                <label class="control-label"> Código Postal</label>
+                <input style="margin-left: 5%" type="text" class="form-control" name="c_postal" value="" data-rule-number="true" maxlength="5" placeholder="Escribe un Código Postal Válido" required/>
+            </div>
+        </div>
+        <div class="input-group" style="margin-top: 1%">
+            <div class="form-group f98 required inline" data-fid="f98" name="idioma">
+                <label class="control-label" for="f98">Idioma</label>
+            <div class="radio">
+                    <input  id="f97_1" name="idioma"  type="radio" value="espanyol">
+                    <label  for="f97_1">
+                        Español
+                    </label><br>
+                    <input  id="f97_2" name="idioma"  type="radio" value="valenciano"  >
+                    <label  for="f97_2">
+                        Valenciano
+                    </label><br>
+                    <input  id="f97_3" name="idioma"  type="radio" value="ingles"  >
+                    <label  for="f97_3">
+                        Ingles
+                    </label>
+                </div>
+            </div>
+            <div style="margin-left: 5%" class="form-group f98 required inline" data-fid="f98" name="segmento">
+                <label class="control-label" for="f98">Segmento</label>
+            <div class="radio">
+                    <input  id="f97_1" name="segmento"  type="radio" value="residencial">
+                    <label  for="f97_1">
+                        Residencial
+                    </label><br>
+                    <input  id="f97_2" name="segmento"  type="radio" value="especial"  >
+                    <label  for="f97_2">
+                        Especial
+                    </label><br>
+                    <input  id="f97_3" name="segmento"  type="radio" value="autonomo"  >
+                    <label  for="f97_3">
+                        Autónomo
+                    </label><br>
+                    <input  id="f97_4" name="segmento"  type="radio" value="empresa"  >
+                    <label  for="f97_4">
+                        Empresa
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div>
+            <button class="btn btn-primary" type="submit">Agregar</button>
+        </div>
+    </form>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
