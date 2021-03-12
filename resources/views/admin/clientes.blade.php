6f30b4b37130f07e10ff2a8aa3c9bcd919dc79de
nuvolcrm <80329034+nuvolcrm@users.noreply.github.com>
2021-03-12 21:55:02 +0100

    Update clientes.blade.php

--- resources/views/admin/clientes.blade.php
+++ resources/views/admin/clientes.blade.php
@@ -57,7 +57,7 @@ $clientes = DB::table('cliente')->get();
                             <td><?php echo $row -> dni ?></td>
                             <td><?php echo $row -> nombre,' ',$row-> apellido1,' ',$row-> apellido2?></td>
                             <!--<td><img width="100%" src="{{asset('/vendor/adminlte/dist/img/idiomas/<?php echo $row-> idioma?>.png')}}"></td>-->
-                            <td><img width="100%" src="'./public/vendor/adminlte/dist/img/idiomas/<?php echo $row-> idioma?>.png'"></td>
+                            <td><img width="100%" src="'public/vendor/adminlte/dist/img/idiomas/<?php echo $row-> idioma?>.png'"></td>
                             <td><?php echo $row-> alias?></td>
                             <td><a class='fas fa-phone-square-alt' href=" tel:<?php echo $row-> telefono?>"></td>
                             <td><?php echo $row-> telefono?></td>
