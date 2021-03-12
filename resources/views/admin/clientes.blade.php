2c890d86638f6598f4492fa46859c2c5eeb2a729
nuvolcrm <80329034+nuvolcrm@users.noreply.github.com>
2021-03-12 11:55:12 +0100

    لا no
    contraseña

--- resources/views/admin/clientes.blade.php
+++ resources/views/admin/clientes.blade.php
@@ -10,7 +10,7 @@
 </div>
 @stop
 <?php
-    $link = new PDO('mysql:host=138.68.138.156;dbname=nuvolcrm', 'forge', 'sBeywveCYpdkgajGCFXNSsssssss');
+    $link = new PDO('mysql:host=138.68.138.156;dbname=nuvolcrm', 'forge', 'sBeywveCYpdkgajGCFXNSssssss');
 ?>
 @section('content')
     <body>
