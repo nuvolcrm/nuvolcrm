8248c7c6fc673bb061787897af7888a1b83e78aa
soufiane raki <soufiane@hackermail.com>
2021-04-21 17:19:08 +0200

    pedidos datatable

--- resources/views/extra/pedidos/index.blade.php
+++ resources/views/extra/pedidos/index.blade.php
@@ -97,34 +97,5 @@
     <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.7/js/responsive.bootstrap4.min.js"></script>
-    <script>
-        $('#example2').DataTable({
-            responsive: true,
-            autoWidth: false,
-            "order": [
-                [11, "desc"]
-            ],
-            "language": {
-                "lengthMenu": "Ver " +
-                    `<select class = "custom-select custom-select-sm form-control form-control-sm">
-                                <option value = '5'>5</option>
-                                <option value = '10'>10</option>
-                                <option value = '25'>25</option>
-                                <option value = '50'>50</option>
-                                <option value = '-1'>All</option>
-                              </select>` +
-                    " registros",
-                "zeroRecords": "Ninguna coincidencia",
-                "info": "Página _PAGE_ de _PAGES_",
-                "infoEmpty": "Sin registros disponibles",
-                "infoFiltered": "(filtrado de _MAX_ registros totales)",
-                'search': 'Buscar',
-                'paginate': {
-                    'next': 'Siguiente',
-                    'previous': 'Anterior'
-                }
-            }
-        });
-
-    </script>
+    <script src="/js/datatable.js"></script>
 @stop
