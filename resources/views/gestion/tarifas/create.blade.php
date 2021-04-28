17f0faf57e5953d87d8dd9ee44a90a87bc5a73e7
nuvolcrm <hola@movy.es>
2021-04-28 17:57:44 +0200

    --

--- resources/views/gestion/tarifas/create.blade.php
+++ resources/views/gestion/tarifas/create.blade.php
@@ -69,6 +69,7 @@
                         </div>
                     </div>
                 </div>
+                <!-- </div> -->
                     <div class="col-md-4">
                         <div class="box-body">
                             <div class="box-body">
